d765a22918c179bbe876f956665d03625a136d31
Eunovo <eunovo9@gmail.com>
2021-08-20 20:28:18 +0100

    style auth page for bigger screens

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage, Form, Formik, Field } from 'formik';
 import { Link } from 'react-router-dom';
 import * as yup from 'yup';
+import PeoplePodcastImage from './people-podcast.webp';
 import './auth.css';
 
 const validationSchema = yup.object({
@@ -13,41 +14,49 @@ const validationSchema = yup.object({
 export const Login: React.FC = () => {
     const onSubmit = () => { };
 
-    return <div className='container auth'>
-        <h1>Demo</h1>
+    return <div className='auth'>
 
-        <div className='auth__form'>
-            <h3>Welcome Back!</h3>
-            <div className='text-small'>
-                Please log in to your account
-            </div>
+        <div className='auth__left'>
+            <h1>Demo</h1>
 
-            <Formik
-                initialValues={{ email: '', password: '' }}
-                validationSchema={validationSchema}
-                onSubmit={onSubmit}
-            >
-                <Form>
-                    <Field name='email' />
-                    <div className='input__error'>
-                        <ErrorMessage name='email' />
-                    </div>
-                    
-                    <Field type='password' name='password' />
-                    <div className='input__error'>
-                        <ErrorMessage name='password' />
-                    </div>
+            <div className='auth__form'>
+                <h3>Welcome Back!</h3>
+                <div className='text-small'>
+                    Please log in to your account
+                </div>
 
-                    <button className='btn-primary' type='submit'>
-                        Login
-                    </button>
-                </Form>
-            </Formik>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}
+                >
+                    <Form>
+                        <Field name='email' />
+                        <div className='input__error'>
+                            <ErrorMessage name='email' />
+                        </div>
 
-            <div className='text-small auth__form__footer'>
-                <div>You don't have an account? <Link className='link-primary' to='/signup'>Create an account</Link></div>
-                <div>Forgot password? <Link className='link-primary' to='/forgot-password'>Forgot Password</Link></div>
+                        <Field type='password' name='password' />
+                        <div className='input__error'>
+                            <ErrorMessage name='password' />
+                        </div>
+
+                        <button className='btn-primary' type='submit'>
+                            Login
+                        </button>
+                    </Form>
+                </Formik>
+
+                <div className='text-small auth__form__footer'>
+                    <div>You don't have an account? <Link className='link-primary' to='/signup'>Create an account</Link></div>
+                    <div>Forgot password? <Link className='link-primary' to='/forgot-password'>Forgot Password</Link></div>
+                </div>
             </div>
         </div>
+
+        <div className='auth__right'>
+            <img src={PeoplePodcastImage} alt='people-podcast' />
+        </div>
+
     </div>
 }
